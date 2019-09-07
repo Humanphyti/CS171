edb1279765aad4729290f9a22d202090a058b425
Humanphyti <humanphyti@gmail.com>
2019-09-07 14:57:04 -0700

    Math tutor push

---
+++ Semester Project Math Tutor/Sprint 1 (SP-A)/Program description/Program description/Description Display.cpp
@@ -0,0 +1,22 @@
+/////////////////////////
+// Program Name: Math Tutor Description
+// Name: Eben Schumann
+// Date: 2/15/2016
+// Class and Section: CS 171 SEC 3
+/////////////////////////
+
+// preprocessor code
+#include <iostream>
+using namespace std;
+
+// Main code
+int main() {
+	// output the title of the program
+	cout << "Hello! I am your electronic, computer based, mathematics tutor!\n" << endl;
+	// user input preferred name
+	cout << "How do you want me to address you? Name:\n" << endl;
+	// output the description of the Program
+	cout << "The sole purpose of my existenece is to help you learn this wonderful thing called mathematics of the basic variety.\n" << endl;
+	// Output what the discipline and subjects covered
+	cout << "By the end of my use I hope that you will be able to feel confident in multiplication, division, addition, subtraction, and order of operations involving some or all of the previously mentioned operations, which has the potential to include the Quadratic Formula." << endl;
+}
